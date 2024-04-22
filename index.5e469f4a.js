0bcd43123f30cd4c89a8a4a6abb21a4434e0dae7
Gigleaz-Repo <saitejagummala6@gmail.com>
2024-04-22 10:29:25 +0530

    first commit

---
+++ index.5e469f4a.js
@@ -0,0 +1,19 @@
+// function toggleNav() {
+//   var navLinks = document.querySelector(".nav-links");
+//   navLinks.classList.toggle("active");
+// }
+function toggleNav() {
+    var navLinks = document.querySelector(".nav-links");
+    var hamburger = document.querySelector(".hamburger");
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("cross");
+    if (hamburger.classList.contains("cross")) {
+        hamburger.innerHTML = '<i class="fas fa-times"></i>';
+        hamburger.style.transform = "rotate(45deg)";
+    } else {
+        hamburger.innerHTML = '<i class="fas fa-bars"></i>';
+        hamburger.style.transform = "rotate(0deg)";
+    }
+}
+
+//# sourceMappingURL=index.5e469f4a.js.map
